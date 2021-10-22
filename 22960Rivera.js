f3dc85262baac34f5080e2a2d95d687ed4c61125
yoryao <jorgejoaquinrivera@gmail.com>
2021-10-22 00:25:12 -0300

    Dom modified - inform

--- 22960Rivera.js
+++ 22960Rivera.js
@@ -15,8 +15,13 @@ class Prestamo {
       
 // PRESENTACION DE LA INFORMACION SIN MANIPULAR.
     presentar() {
-        console.log(`SOLICITUD DEL CLIENTE: ${this.nombre} solicita ${this.monto} $ en ${this.cuotas} cuotas.`)
-    };
+        console.log(`SOLICITUD DEL CLIENTE: ${this.nombre} solicita ${this.monto} $ en ${this.cuotas} cuotas.`);
+        let montoDom = document.getElementById("montoInicialInforme");
+        montoDom.value = this.monto;
+        let cuotasDom = document.getElementById("cuotasInforme");
+        cuotasDom.value = this.cuotas;
+    
+      };
 
 // DECLARAR VALOR DE INDICE EN BASE A CUOTAS.
     calcularInteres() {
@@ -29,11 +34,15 @@ class Prestamo {
       } else {
         console.log(`Ingrese una cantidad de cuotas valida.`);
       };
+      let interesDom = document.getElementById("interesInforme");
+      interesDom.value = this.indice;    
     };
 
 //CALCULAR EL MONTO FINAL DEL PRESTAMOS
     calcularMontoFinal() {
         this.final = this.monto * this.indice;
+        let finalDom = document.getElementById("finalInforme");
+        finalDom.value = this.final;
     };    
 
 //SE SOLICITA EL PRESTAMO, Y SE INGRESA EL DNI EN LA LISTA DE DEUDORES. 
@@ -53,18 +62,18 @@ prestamo1.calcularInteres();
 prestamo1.calcularMontoFinal();
 prestamo1.generar();
 
-prestamo2.calcularInteres();
-prestamo2.calcularMontoFinal();
-prestamo2.generar();
+// prestamo2.calcularInteres();
+// prestamo2.calcularMontoFinal();
+// prestamo2.generar();
 
-prestamo3.calcularInteres();
-prestamo3.calcularMontoFinal();
-prestamo3.generar();
+// prestamo3.calcularInteres();
+// prestamo3.calcularMontoFinal();
+// prestamo3.generar();
 
 //PRESENTACIONES
 prestamo1.presentar();
-prestamo2.presentar();
-prestamo3.presentar();
+// prestamo2.presentar();
+// prestamo3.presentar();
 
 const informeDeudores = `Actualmente hay ${dniDeudores.length} deudores. Sus DNI son ${dniDeudores.join(", ")}.`;
 console.log(informeDeudores);
