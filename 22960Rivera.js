a816c0a9975858f7138aa2aa605e6317df6d85d8
yoryao <jorgejoaquinrivera@gmail.com>
2021-10-27 21:28:43 -0300

    Review for discord

--- 22960Rivera.js
+++ 22960Rivera.js
@@ -1,5 +1,3 @@
-
-
 //INCORPORAR UN ARRAY : REQUISITO DE LA ENTREGA.
 const dniDeudores = [];
 const deudores = [];
@@ -18,17 +16,13 @@ class Prestamo {
 
   // PRESENTACION DE LA INFORMACION SIN MANIPULAR.
   presentar() {
-    //IMPRIMO POR CONSOLA PARA VERIFICAR FLUJO
-    console.log(
-      `SOLICITUD DEL CLIENTE: ${this.nombre} solicita ${this.monto} $ en ${this.cuotas} cuotas.`
-    );
-
     //PRESENTO LA INFORMACIÓN SOBRE EL HTML A TRAVEZ DEL DOM.
     let presentacion = document.getElementById("presentar");
     presentacion.innerText = `SOLICITUD DEL CLIENTE: ${this.nombre} solicita ${this.monto} $ en ${this.cuotas} cuotas.`;
   }
 
   // DECLARAR VALOR DE INDICE EN BASE A CUOTAS.
+  // VALIDAR INGRESO ERROR EN CUOTAS - USAR SWITCH
   calcularInteres() {
     if (this.cuotas == 1) {
       this.indice = 1.1;
@@ -37,7 +31,6 @@ class Prestamo {
     } else if (this.cuotas == 3) {
       this.indice = 1.3;
     } else {
-      console.log(`Ingrese una cantidad de cuotas valida.`);
       alert(`Ingrese una cantidad de cuotas valida.`);
     }
   }
@@ -47,54 +40,61 @@ class Prestamo {
     this.final = this.monto * this.indice;
 
     const div = document.getElementById("int&Monto");
-        div.innerHTML = `  <label>Interes:</label><br>
+    div.innerHTML = `  <label>Interes:</label><br>
                            <input type="number" name="interesInforme" id="interesInforme" value="${this.indice}"readonly><br>
                            <label>Monto Final:</label><br>
                           <input type="number" name="finalInforme"
-    id="finalInforme" value="${this.final}" readonly>`
-
+    id="finalInforme" value="${this.final}" readonly>`;
   }
 
   //SE SOLICITA EL PRESTAMO, Y SE INGRESA EL DNI EN LA LISTA DE DEUDORES.
   //ACA ESTOY TIRANDO FRUTA, PORQUE ME CARGA 2 VECES EL DNI Y NO SE PORQUE.
+  //for discord
   generar() {
     if (!dniDeudores.includes(this.dni)) {
       dniDeudores.push(this.dni);
-      deudores.push(this.prestamo);
-     this.deudor = true;
-     } else {
-      console.log("Ya esta ingresado al registro de deudores. No otorgar crédito");
-    };
-
-};
-};
-
+      console.log(this.dni);
+      console.log(dniDeudores);
+      console.log("No esta ingresado, Otorgar Crédito.");
+    } else {
+      console.log(this.dni);
+      console.log(
+        "Ya esta ingresado al registro de deudores. No otorgar crédito"
+      );
+    }
+  }
+}
 
 //MANEJO DE EVENTOS A TRAVES DE LOS BOTONES.
-  //FUNCIONES
+//FUNCIONES
 
 function solicitar() {
   let nombre = document.getElementById("nombre").value;
   let dni = document.getElementById("dni").value;
   let monto = document.getElementById("monto").value;
   let cuotas = document.getElementById("cuotas").value;
-  
+
   const prestamo = new Prestamo(nombre, dni, monto, cuotas);
-  
+
   prestamo.calcularInteres();
   prestamo.calcularMontoFinal();
   prestamo.presentar();
-  
+
   //ESTOY LEVANTANDO LOS DATOS DEL PRIMER FORMULARIO
   let boton2 = document.getElementById("generar");
   boton2.addEventListener("click", generar);
-  
+
+  //INGRESO EL OBJETO CREADO EN EL ARRAY DE OBJETOS - DEUDORES
+
   function generar() {
     prestamo.generar();
-    console.log(dniDeudores);
+
+    deudores.push(prestamo);
+    // console.log(deudores);
+    //prestamo.generar();
+    //console.log(dniDeudores);
   }
 }
-
 
 function mostrarDeudores() {
   let tabla = document.getElementById("deudores");
@@ -104,13 +104,12 @@ function mostrarDeudores() {
     let fila = document.createElement("li");
     fila.innerHTML = dniDeudores[i];
     tabla.appendChild(fila);
-    
   }
 }
 
 //BOTONES + EVENTOS
 
-let boton = document.getElementById("submitDatos");
+let boton = document.getElementById("calcular");
 boton.addEventListener("click", solicitar);
 
 let boton3 = document.getElementById("mostrarDeudores");
